9e8060263b7449e4c203650e5af70432e395ad77
pranavsource1 <pranavyadav251@gmail.com>
2026-02-08 00:15:12 +0530

    live site

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: 'https://chemical-equipment-parameter-visualizer-hx5i.onrender.com/api/data',
     timeout: 10000,
 });
 
